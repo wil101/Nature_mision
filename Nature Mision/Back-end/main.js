ec815e7cd5e8fbbbb6e581639163e0ce71887567
Wilmar Osorio <101955537+wil101@users.noreply.github.com>
2023-09-19 15:05:19 -0500

    monte de la arquitectura del proyecto

---
+++ Nature Mision/Back-end/main.js
@@ -0,0 +1,9 @@
+function iniciarJuego() {
+    // Redirigir a la página de inicio del juego
+    window.location.href = "index.html";
+}
+
+function verReglas() {
+    // Redirigir a la página de reglas del juego
+    window.location.href = "Reglas.html";
+}
