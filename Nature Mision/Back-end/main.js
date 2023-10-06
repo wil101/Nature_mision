7a72d133d633c581c74498bf1c50883fbb55aeda
Wilmar Osorio <101955537+wil101@users.noreply.github.com>
2023-10-06 08:36:39 -0500

    Delete Nature Mision/Back-end/main.js

--- Nature Mision/Back-end/main.js
+++
@@ -1,9 +0,0 @@
-function iniciarJuego() {
-    // Redirigir a la página de inicio del juego
-    window.location.href = "index.html";
-}
-
-function verReglas() {
-    // Redirigir a la página de reglas del juego
-    window.location.href = "Reglas.html";
-}
